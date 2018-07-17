463edccf4a848b89e4eecce94df0b805057753a1
Terence Lee <hone02@gmail.com>
2018-07-17 16:51:49 -0500

    write local:build test

--- test/commands/local/build.test.ts
+++ test/commands/local/build.test.ts
@@ -1,6 +1,29 @@
 import {expect, test} from '@oclif/test'
 import * as fs from 'fs'
+const child = require('child_process')
 const tmp = require('tmp')
 
 describe('local:build', () => {
+  test
+    .add('cwd', () => process.cwd())
+    .add('tmpdir', () => {
+      let tmpdir = tmp.dirSync({unsafeCleanup: true, prefix: 'heroku_local_'})
+      process.chdir(tmpdir.name)
+      return tmpdir
+    })
+    .do(ctx => {
+      child.spawnSync("git", ["clone", "https://github.com/heroku/node-js-getting-started", "."])
+    })
+    .add('app', () => {
+      return "foo"
+    })
+    .command(['local:build', '--app=foo'])
+    .finally(ctx => {
+      ctx.tmpdir.removeCallback()
+      process.chdir(ctx.cwd)
+    })
+    .it('bootstraps new buildpack', (ctx) => {
+      expect(fs.existsSync(`${ctx.app}.slug`)).to.equal(true)
+      expect(fs.existsSync(`.${ctx.app}.cache`)).to.equal(true)
+    })
 })
